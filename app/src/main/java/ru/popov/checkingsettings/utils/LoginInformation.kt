05bf03a8dec96e93dae18139ca19198810eaec64
DmitriyPopovRT <radiotexnika.popov.d@mail.ru>
2021-12-30 11:07:57 +0300

    CheckingSettings v4

--- app/src/main/java/ru/popov/checkingsettings/utils/LoginInformation.kt
+++ app/src/main/java/ru/popov/checkingsettings/utils/LoginInformation.kt
@@ -3,8 +3,8 @@ package ru.popov.checkingsettings.utils
 object LoginInformation {
     const val USER = "OTK_rw"
     const val PASS = "ZYZbnrGvOOxuQMJxR1B1"
-    const val PATH = "smb://GO_CRASH/Public/ОТК"
-//    const val PATH = "smb://GO_CRASH/Public/ОТК/Debug"
+    const val PATH = "smb://GO_CRASH/Public/ОТК/Проверка настроек"
+//    const val PATH = "smb://GO_CRASH/Public/ОТК/Debug/Проверка настроек"
     const val NAME_FILE = "settings"
     const val ADMIN_PASSWORD = "12345"
 }
